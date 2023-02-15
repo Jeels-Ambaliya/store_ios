33b8705edcc898afec157880567480598311318d
Jeels-Ambaliya <19bmiit062@gmail.com>
2023-02-15 13:57:50 +0530

    Initial commit

---
+++ lib/screens/model/globals.dart
@@ -0,0 +1,129 @@
+class Globals {
+  static List<Map<String, dynamic>> ProductsList = [
+    {
+      'Title': "Zara Shirt",
+      'Img': "assets/images/shirt1.png",
+      'price': "1500",
+    },
+    {
+      'Title': "Zara Pant",
+      'Img': "assets/images/pant1.png",
+      'price': "2199",
+    },
+    {
+      'Title': "FabIndia Kurti",
+      'Img': "assets/images/kurti1.png",
+      'price': "1699",
+    },
+    {
+      'Title': "Antheaa Onepiece",
+      'Img': "assets/images/onepiece1.png",
+      'price': "2599",
+    },
+    {
+      'Title': "Allen Solly Shirt",
+      'Img': "assets/images/shirt2.png",
+      'price': "1899",
+    },
+    {
+      'Title': "Allen Solly Pant",
+      'Img': "assets/images/pant2.png",
+      'price': "2466",
+    },
+    {
+      'Title': "Rangriti Kurti",
+      'Img': "assets/images/kurti2.png",
+      'price': "1299",
+    },
+    {
+      'Title': "Berrylush Onepiece",
+      'Img': "assets/images/onepiece2.png",
+      'price': "2199",
+    },
+    {
+      'Title': "Raymond Shirt",
+      'Img': "assets/images/shirt3.png",
+      'price': "2499",
+    },
+    {
+      'Title': "Raymond Pant",
+      'Img': "assets/images/pant3.png",
+      'price': "6599",
+    },
+    {
+      'Title': "Varanga Kurti",
+      'Img': "assets/images/kurti3.png",
+      'price': "1999",
+    },
+    {
+      'Title': "Trendy Divya Onepiece",
+      'Img': "assets/images/onepiece3.png",
+      'price': "2999",
+    },
+  ];
+
+  static List<Map<String, dynamic>> OnepieceList = [
+    {
+      'Title': "Antheaa Onepiece",
+      'Img': "assets/images/onepiece1.png",
+      'price': "2599",
+    },
+    {
+      'Title': "Berrylush Onepiece",
+      'Img': "assets/images/onepiece2.png",
+      'price': "2199",
+    },
+    {
+      'Title': "Trendy Divya Onepiece",
+      'Img': "assets/images/onepiece3.png",
+      'price': "2999",
+    },
+    {
+      'Title': "Tokyo Talkies Onepiece",
+      'Img': "assets/images/onepiece4.png",
+      'price': "2155",
+    },
+    {
+      'Title': "AASK Onepiece",
+      'Img': "assets/images/onepiece5.png",
+      'price': "4500",
+    },
+    {
+      'Title': "Slenor Onepiece",
+      'Img': "assets/images/onepiece6.png",
+      'price': "5600",
+    },
+    {
+      'Title': "Tokyo Talkies Onepiece",
+      'Img': "assets/images/onepiece7.png",
+      'price': "3699",
+    },
+    {
+      'Title': "Berrylush Onepiece",
+      'Img': "assets/images/onepiece8.png",
+      'price': "4500",
+    },
+  ];
+
+  static DateTime Date_Time = DateTime.now();
+  static List<dynamic> Months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "April",
+    "May",
+    "Jun",
+    "July",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  static List CartList = [];
+  static String? Title;
+  static String? Img;
+  static String? Price;
+  static int TotalPrice = 0;
+}
